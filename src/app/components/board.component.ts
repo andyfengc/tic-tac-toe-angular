7925048902c5f34a62b0561d57b05c41257794b5
Owner <andyfengc@gmail.com>
2025-04-26 23:28:30 -0400

    calculate winner

--- src/app/components/board.component.ts
+++ src/app/components/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Square } from "./square";
 
 @Component({
@@ -8,21 +8,57 @@ import { Square } from "./square";
     templateUrl: `board.component.html`,
     styleUrl: '../app.component.css'
 })
-export class Board{
-    public squares : string[] = ['','','','','','','','',''];
-    public xIsNext : boolean = true;
+export class Board implements OnInit {
+    public squares: string[] = ['', '', '', '', '', '', '', '', ''];
+    public xIsNext: boolean = true;
 
-    public handleClick(i : number){
-        if (this.squares[i]) return;
+    // show status
+    public status: string = '';
+    public showStatus() {
+        const winner = this.calculateWinner(this.squares);
+        if (winner) {
+            this.status = "Winner: " + winner;
+        } else {
+            this.status = "Next player: " + (this.xIsNext ? "X" : "O");
+        }
+    }
+ 
+    ngOnInit(): void {
+        this.showStatus();
+    }
+
+    public handleClick(i: number) {
+        if (this.squares[i] || this.calculateWinner(this.squares)) return;
         const nextSquares = this.squares.slice();
-        if (this.xIsNext){
+        if (this.xIsNext) {
             nextSquares[i] = "X";
         }
-        else{
+        else {
             nextSquares[i] = "O";
         }
         this.squares = nextSquares;
         this.xIsNext = !this.xIsNext;
+        this.showStatus();
         // this.squares[i] = "X";
+    }
+
+    calculateWinner(squares: string[]) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
 }
